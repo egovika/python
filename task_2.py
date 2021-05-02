455ae2133fcacdccd924f5789cd6e886885992d5
ego.vika <ego.vika@icloud.com>
2021-05-02 15:57:40 +0300

    Task 1

---
+++ task_2.py
@@ -0,0 +1,88 @@
+
+1
+
+
+my_list = [30, None, -16, 'False', False, 7.5]
+def my_type(el):
+    for el in range(len(my_list)):
+        print(type(my_list[el]))
+    return
+my_type(my_list)
+
+
+2
+
+
+el_count = int(input('Введите количество элементов списка -'))
+my_list = []
+i = 0
+el = 0
+while i < el_count:
+    my_list.append(input('Введите следующее значение списка -'))
+    i += 1
+for elem in range(int(len(my_list)/2)):
+    my_list[el], my_list[el + 1] = my_list [el + 1], my_list[el]
+print(my_list)
+
+
+3
+
+seasons_list = ['winter', 'spring', 'summer', 'autumn']
+seasons_dict = {1: 'winter', 2: 'spring', 3: 'summer', 4: 'autumn'}
+month = int(input("Enter month by the number: "))
+if month == 1 or month == 12 or month == 2:
+    print(seasons_dict.get(1))
+    print(seasons_list[0])
+
+elif month == 3 or month == 4 or month == 5:
+    print(seasons_dict.get(2))
+    print(seasons_list[1])
+
+elif month == 6 or month == 7 or month == 8:
+    print(seasons_dict.get(3))
+    print(seasons_list[2])
+
+elif month == 9 or month == 10 or month == 11:
+    print(seasons_dict.get(4))
+    print(seasons_list[3])
+
+else:
+    print("This moth is not exist")
+
+4
+
+my_str = input("Enter string- ")
+my_word = []
+num = 1
+for el in range(my_str.count(' ') + 1):
+    my_word = my_str.split()
+    if len(str(my_word)) <= 10:
+        print(f" {num} {my_word [el]}")
+        num += 1
+    else:
+        print(f"{num} {my_word [el] [0:10]}")
+        num += 1
+
+
+5
+
+my_list = [7, 5, 3, 3, 2]
+print(f"Рейтинг - {my_list}")
+digit = int(input("Введите число (111 - выход) "))
+while digit != 111:
+    for el in range(len(my_list)):
+        if my_list[el] == digit:
+            my_list.insert(el + 1, digit)
+            break
+        elif my_list[0] < digit:
+            my_list.insert(0, digit)
+        elif my_list[-1] > digit:
+            my_list.append(digit)
+        elif my_list[el] > digit and my_list[el + 1] < digit:
+            my_list.insert(el + 1, digit)
+    print(f"Текущий список - {my_list}")
+    digit = int(input("Введите число: "))
+
+
+6
+
